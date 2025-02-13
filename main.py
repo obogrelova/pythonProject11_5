b892bf15080dd99c6c2d26939cca7df3b99db682
obogrelova <on.obogrelova@gmail.com>
2025-02-13 23:31:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
-from deep_translator import GoogleTranslator
+from translate import Translator
 import requests
 
 from config import TOKEN, THE_API_KEY
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+translator = Translator()
 
-class Form(StatesGroup):
-    waiting_for_text = State()
-    waiting_for_translation = State()
 
 def get_fox_types():
     url = 'https://api.api-ninjas.com/v1/animals?name=fox'
@@ -43,12 +39,6 @@ async def send_fox_info(message: Message):
         await message.answer(info_text)
     else:
         await message.answer('Тип не найден. Попробуйте ещё раз.')
-
-@dp.message(Form.waiting_for_translation)
-async def handle_translation(message:Message, state: FSMContext):
-    text_to_translate = message.text
-    translator = GoogleTranslator(sourse='auto', target='en')
-    translated_text = translator.translate(text_to_translate)
 
 
 async def main():
